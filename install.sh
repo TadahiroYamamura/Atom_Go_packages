bd5c8114c02da7a55c2cc72bc6c5d04b09e35775
TadahiroYamamura <mt.village12@gmail.com>
2019-06-15 13:32:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ go get -u github.com/tpng/gopkgs
 go get -u github.com/ramya-rao-a/go-outline
 go get -u github.com/motemen/gore/cmd/gore
 go get -u github.com/derekparker/delve/cmd/dlv
+go get -u golang.org/x/tools/cmd/godoc
 
 apm install atom-ide-ui
 apm install go-plus
