2d50eb0691a15923c55fff9be4703f8cff721df5
TadahiroYamamura <mt.village12@gmail.com>
2019-06-15 23:33:11 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/gorename
-go get -u github.com/sqs/goreturns
-go get -u github.com/mdempsky/gocode
-go get -u github.com/mgechev/revive
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-go get -u github.com/zmb3/gogetdoc
-go get -u github.com/zmb3/goaddimport
-go get -u github.com/rogpeppe/godef
-go get -u golang.org/x/tools/cmd/guru
-go get -u github.com/fatih/gomodifytags
-go get -u github.com/tpng/gopkgs
-go get -u github.com/ramya-rao-a/go-outline
 go get -u github.com/motemen/gore/cmd/gore
 go get -u github.com/derekparker/delve/cmd/dlv
-go get -u golang.org/x/tools/cmd/godoc
 
 apm install atom-ide-ui
 apm install go-plus
 apm install go-debug
 apm install go-signature-statusbar
+
+# ATOM > Packages > Go > Update Tools
