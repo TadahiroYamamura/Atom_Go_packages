5cb317349dca082dd6139855665a1766b1937256
TadahiroYamamura <mt.village12@gmail.com>
2019-06-15 23:19:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
 go get -u github.com/sqs/goreturns
 go get -u github.com/mdempsky/gocode
-go get -u github.com/alecthomas/gometalinter
 go get -u github.com/mgechev/revive
 go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u github.com/zmb3/gogetdoc
